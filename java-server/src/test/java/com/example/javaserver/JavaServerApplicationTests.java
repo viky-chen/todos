9fe2cc9cd3f98280a8f92c293d1ef01a834e66ab
chenyunqi <viky.chen@outlook.com>
2024-12-01 00:33:23 +0800

    Refactor project structure and rename Kotlin Todo to kotlin-todo

---
+++ java-server/src/test/java/com/example/javaserver/JavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
