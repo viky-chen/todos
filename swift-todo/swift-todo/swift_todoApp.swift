a233efd6f494378ca080b14f5c775f9675a2273a
chenyunqi <viky.chen@outlook.com>
2024-11-30 22:27:43 +0800

    add swift-todo

---
+++ swift-todo/swift-todo/swift_todoApp.swift
@@ -0,0 +1,19 @@
+//
+//  swift_todoApp.swift
+//  swift-todo
+//
+//  Created by 陈运棋 on 2024/11/27.
+//
+
+import SwiftUI
+
+@main
+struct swift_todoApp: App {
+    /// <#Description#>
+    var body: some Scene {
+        
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
